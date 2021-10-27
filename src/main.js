6a02c513f214af20ce701354b8838bd2cc16bbb5
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-10-27 13:56:48 +0800

    feat: Add buefy dependency and init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes.js'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Buefy)
 
 const router = new VueRouter({
   routes: Routes,
