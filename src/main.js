f06bda547940676eaba5836378a13f76ff0358d9
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-10-27 12:37:45 +0800

    fix: Fix router connection

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter  from 'vue-router'
-import Routes from './routes'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: Routes,
