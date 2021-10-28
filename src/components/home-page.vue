0c9c3290bf470b23855ce31d0ac6387669babb67
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-10-28 11:29:05 +0800

    feat: Add dynamic card rendering

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <div class="content">
+          <h1 class="is-size-1 has-text-left">Hello Jianzhen</h1>
+        </div>
+        <div>
+          <input v-model="title" type="text" placeholder="Enter title">
+            <button class="button" @click="addCategory()">+</button>
+        </div>
+      </div>
+    </div>
+    <div class="columns" id="category">
+      <div class="column is-one-quarter" v-for="(category, index) in categories" :key="index">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img :src="category.img" alt="image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">{{ category.title }}</p>
+                <button>Delete</button>
+              </div>
+            </div>
+          </div>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function(){
+    return{
+      title: '',
+      categories: [
+        {
+          title: 'First title',
+          img: 'https://bulma.io/images/placeholders/1280x960.png'
+        },
+    ]}
+  },
+  methods: {
+    addCategory(){
+      if(this.title.length === 0){
+        return;
+      }
+      else{
+        this.categories.push({
+          title: this.title,
+          img: 'https://bulma.io/images/placeholders/1280x960.png'
+        })
+      }
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
